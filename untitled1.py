ed35b3d5fd423d691b3b5db03584f683577ac05f
Sai Vinay Varikooty <sv.varikooty@mail.utoronto.ca>
2019-01-19 21:50:26 -0500

    Deleted old files, added new ones.

---
+++ untitled1.py
@@ -0,0 +1,37 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Landing Page!'
+
+@app.route('/complicated')
+def complicated_life():
+    pass
+
+@app.route('/process_image')
+def process_image():
+    pass
+
+@app.route('choose-object')
+def choose_object():
+    pass
+
+if __name__ == '__main__':
+    app.run()
+
+
+## Need to process image from POST request
+##
+## Send image to Azure API -> Separate module: Sai
+##
+## Filter objects using another module -> Separate module: Cynthia
+##
+## Pick top three objects and see where they are supposed to go?
+##
+## Alex figures out HTML / CSS template so we know how to route back? 
+##
+##
+##
